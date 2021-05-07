b27f60548fc5acf2cab21c3899785fef645ff8ec
betweenhj702 <75344320+betweenhj702@users.noreply.github.com>
2021-05-07 16:23:21 +0900

    11. 브루트 포스, 1번 블랙잭

---
+++ 2798.java
@@ -0,0 +1,61 @@
+/*
+문제
+카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 블랙잭은 카지노마다 다양한 규정이 있다.
+
+한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+
+김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+
+이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+
+N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+
+입력
+첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+
+합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+
+출력
+첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+
+sol)
+1. 배열을 만들어 주어진 숫자를 입력하고 
+2. 배열에서 3개를 선택해 더하고 M을 넘지않는 값을 최대값 변수에 저장한다.
+    2-1) (0,1,2 ....) (0,2,3 ....) 이런식으로 반복문을 짜면 모든 수를 검사할 수 있다.
+    2-2) 마지막 검사 케이스는 (N-2, N-1, N) 이다
+3. 반복하면서 최대값을 구한다.
+
+*/
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main2798 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] caseNum = br.readLine().split(" ");
+        int N = Integer.parseInt(caseNum[0]);
+        int M = Integer.parseInt(caseNum[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] cardNum = new int[N];
+        for(int i=0; i < N; i++){
+            cardNum[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(getMaxNum(N, M, cardNum));
+    }
+
+    static int getMaxNum(int N, int M, int[] cardNum){
+        int maxNum = 0;
+        int tempSum = 0;
+        for(int i = 0; i < N-2; i++){
+            for(int j = i + 1; j < N-1; j++){
+                for(int k = j + 1; k < N; k++){
+                    tempSum = cardNum[i] + cardNum[j] + cardNum[k];
+                    if(tempSum <= M && tempSum > maxNum){
+                        maxNum = tempSum;
+                    }        
+                }
+            }
+        }
+        return maxNum;
+    }
+}
